923dac33598c49aa5343a6414943b122053e631e
Erick Garcia <erick.m.garcia.vega@gmail.com>
2018-02-15 20:54:04 -0600

    adding random-number component

---
+++ app/components/random-number.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import TimeBox from 'time-box';
+
+class RandomNumber extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      random: 0
+    };
+  }
+
+  render() {
+
+
+    return (
+      <div>
+        my rand
+      </div>
+    );
+  }
+}
+
+export default RandomNumber;
